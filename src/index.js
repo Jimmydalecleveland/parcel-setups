78115f6882d44d9b65733bcb623dfe11c01a4e2d
Jimmy DC <jimmydalecleveland@gmail.com>
2021-04-27 20:00:51 -0600

    add sass postcss browserlist css modules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import "./styles/index.scss";
+import styles from "./styles/module-styles.css";
+
+const h2 = document.querySelector("h2");
+h2.classList.add(styles.heading);
+
 const obj = {
   a: "apple crisp",
   b: "buttercream",
