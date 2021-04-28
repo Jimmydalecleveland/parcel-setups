ee66a5ed8eead7d7034e94d42551f7099e726f0a
Jimmy DC <jimmydalecleveland@gmail.com>
2021-04-27 20:40:50 -0600

    add typescript in react

--- src/react/App.tsx
+++ src/react/App.tsx
@@ -23,14 +23,18 @@ const Container = styled.div`
   }
 `;
 
+const Typography: React.FC = ({ children, ...props }) => (
+  <h2 {...props}>{children}</h2>
+);
+
 const App = () => {
   const [state, setState] = useState(false);
 
   return (
     <Container>
-      <h2 onClick={() => setState(!state)}>
+      <Typography onClick={() => setState(!state)}>
         What is state?: {state.toString()}
-      </h2>
+      </Typography>
 
       <a href="/index.html">Home</a>
       <a href="/vue.html">Go to the Vue app</a>
